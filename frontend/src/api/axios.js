741fc4bc849fcf610e6c777ecaa8f32698836771
Dimoon <112808916+Nelo5@users.noreply.github.com>
2026-05-02 15:15:24 +0300

    Use environment variable for API base URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 // src/api/axios.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000'; // Замени на свой URL бэкенда
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';; // Замени на свой URL бэкенда
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
